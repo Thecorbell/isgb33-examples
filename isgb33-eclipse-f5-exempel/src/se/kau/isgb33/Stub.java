a4224466954962141b06d9f1508e00b6632d93ab
Thecorbell <anton.corbell@hotmail.com>
2024-12-05 13:17:37 +0100

    hej

--- isgb33-eclipse-f5-exempel/src/se/kau/isgb33/Stub.java
+++ isgb33-eclipse-f5-exempel/src/se/kau/isgb33/Stub.java
@@ -1,22 +1,31 @@
 package se.kau.isgb33;
 
 import java.io.FileInputStream;
+import java.util.List; 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Properties;
 
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
+import com.mongodb.client.AggregateIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Sorts;
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
@@ -53,22 +62,81 @@ public class Stub {
 					prop.load(input);
 					String connString;
 					connString = prop.getProperty("db.connection_string");
-					logger.info(connString);
+					//logger.info(connString);
 
 					ConnectionString connectionString = new ConnectionString(connString);
 					MongoClientSettings settings = MongoClientSettings.builder().applyConnectionString(connectionString)
 							.build();
 					MongoClient mongoClient = MongoClients.create(settings);
 					MongoDatabase database = mongoClient.getDatabase(prop.getProperty("db.name"));
-					MongoCollection<Document> collection = database.getCollection("test");
+					MongoCollection<Document> collection = database.getCollection("hejhejhej");
 					
-					String country = t.getText();
-					Document myDoc = collection.find(Filters.eq("address.country", country)).first();
+					String tillb = t.getText();
+					Bson filter = Filters.in("amenities", tillb);
+					
+					AggregateIterable<Document> myDocs = collection.aggregate(Arrays.asList(
+							Aggregates.project(
+									Projections.include("name", "address.country", "amenities")
+									
+									),
+							Aggregates.match(filter),
+							Aggregates.limit(3),
+							Aggregates.sort(Sorts.descending("name"))
+							
+							
+							));
+					
+					/* 	 	
+			          String genreInput = t.getText();
+						Bson filter = Filters.in("genres", genreInput);
+						
+						AggregateIterable<Document> myDocs = collection.aggregate(Arrays.asList(
+						    Aggregates.match(filter),
+						    Aggregates.project(
+						        Projections.fields(
+						            Projections.excludeId(),
+						            Projections.include("title", "year")
+						        )
+						    ),
+						    Aggregates.sort(Sorts.descending("title")),
+						    Aggregates.limit(10)
+						));
+*/
+					
+					MongoCursor<Document> iterator = myDocs.iterator();
+					
+					area.setText("");
+					
+					while(iterator.hasNext()) {
+						
+						Document myDoc = iterator.next();
+						
+						
+						area.append("Objektets namn:" + myDoc.getString("name") + "\n");
+						area.append("Land: " + ((Document)myDoc.get("address")).getString("country")+ "\n" );
+						
+						
+						List<String> tillbhlista = (List<String>) myDoc.get("amenities");
+						area.append("Tillbehör: " );
+						for(String til :tillbhlista ) {
+							area.append(til + ", ");
+						}
+						area.append("\n\n ---------------------- \ns");
+					}
+					
+					
+					
+					
+					
+					
+					//Document myDoc = collection.find(Filters.eq("address.country", country)).first();
 					//Bson filter = eq("address.country", "Turkey");
+					
+					
 					
 					//logger.info(myDoc.toJson());
 					
-					area.setText(myDoc.toJson());
+					//area.setText(myDoc.toJson());
 
 				} catch (FileNotFoundException e) {
 					// TODO Auto-generated catch block
